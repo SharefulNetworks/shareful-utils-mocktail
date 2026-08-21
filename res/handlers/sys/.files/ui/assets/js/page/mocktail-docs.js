70557eb2ad00d4d48ff74484277eb0717a330406
Giles Thompson <gilest@hotmail.co.uk>
2026-08-21 18:13:43 +0100

    [L-70]: Continued work on the Mocktail Server; further work on the bundled documentation pages.

--- res/handlers/sys/.files/ui/assets/js/page/mocktail-docs.js
+++ res/handlers/sys/.files/ui/assets/js/page/mocktail-docs.js
@@ -21,6 +21,9 @@
 
     //setup generic back button, will be the same for all instances of the button throughout the docs
     setupBackBtnEventListener();
+
+    //setup getting started category menu buttons
+    setupGettingStartedCategoryEventListeners();
  }
 
 
@@ -54,3 +57,53 @@
         console.error("Back button not found on the page.");
     }
  }  
+
+
+ function setupGettingStartedCategoryEventListeners() {
+
+    const installationCatBtn = document.getElementById('installation-cat-btn');
+    const configCatBtn = document.getElementById('config-cat-btn');
+
+    if(installationCatBtn && configCatBtn){
+        installationCatBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleGettingStartedCategoryContent('installation');
+           
+        });
+
+        configCatBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleGettingStartedCategoryContent('configuration');
+           
+        });
+    }else{
+        console.error("Getting Started category buttons not found on the page.");
+    }
+ }
+
+
+ //toggle the display of the getting started category content  based on the button clicked
+ function toggleGettingStartedCategoryContent(category) {
+    const installationCatBtn = document.getElementById('installation-cat-btn');
+    const configCatBtn = document.getElementById('config-cat-btn');
+    const installationCard = document.getElementById('system-settings-card');
+    const configCard = document.getElementById('security-settings-card');
+
+    // Remove active class from all nav-link elements
+    installationCatBtn.querySelector('.nav-link').classList.remove('active');
+    configCatBtn.querySelector('.nav-link').classList.remove('active');
+
+    if(installationCard && configCard){
+        if (category === 'installation') {
+            installationCard.style.display = 'block';
+            configCard.style.display = 'none';
+             installationCatBtn.querySelector('.nav-link').classList.add('active');
+        } else if (category === 'configuration') {
+            installationCard.style.display = 'none';
+            configCard.style.display = 'block';
+            configCatBtn.querySelector('.nav-link').classList.add('active');
+        }
+    }else{
+        console.error("One or more Getting Started category content cards not found on the page.");
+    }
+ }

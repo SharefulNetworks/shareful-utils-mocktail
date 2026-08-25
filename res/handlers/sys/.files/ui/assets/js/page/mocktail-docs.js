1a4eb8e5ab03dc7c32b6d26f795dd9ba37422d30
Giles Thompson <gilest@hotmail.co.uk>
2026-08-25 13:29:54 +0100

    [L-70]: Continued work on the Mocktail Server; continued on the dynamic webhosting guide which oart of the bundles docs. Began work on the "writing youre first bashlet" section of the dynamic webhosting guide.

--- res/handlers/sys/.files/ui/assets/js/page/mocktail-docs.js
+++ res/handlers/sys/.files/ui/assets/js/page/mocktail-docs.js
@@ -24,6 +24,9 @@
 
     //setup getting started category menu buttons
     setupGettingStartedCategoryEventListeners();
+
+    //setup dynamic web hosting category menu buttons
+    setupDynamicWebHostingCategoryEventListeners();
  }
 
 
@@ -82,6 +85,42 @@
  }
 
 
+ function setupDynamicWebHostingCategoryEventListeners() {
+
+    const introBtn = document.getElementById('dynamic-web-hosting-into-to-bashlets-btn');
+    const writingFirstBashletBtn = document.getElementById('dynamic-web-hosting-writing-first-bashlet-btn');
+    const contextBashletsBtn = document.getElementById('dynamic-web-hosting-context-bashlets-btn');
+    const examplesBashletsBtn = document.getElementById('dynamic-web-hosting-examples-bashlets-btn');
+
+    if(introBtn && writingFirstBashletBtn && contextBashletsBtn && examplesBashletsBtn){
+        introBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleDynamicWebHostingCategoryContent('intro');
+        });
+
+        writingFirstBashletBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleDynamicWebHostingCategoryContent('writing-first-bashlet');
+        });
+
+        contextBashletsBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleDynamicWebHostingCategoryContent('context');
+        });
+
+        examplesBashletsBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            toggleDynamicWebHostingCategoryContent('examples');
+        });
+    }else{
+        console.error("One or more Dynamic Web Hosting category buttons not found on the page.");
+    }
+ }
+
+
+
+
+
  //toggle the display of the getting started category content  based on the button clicked
  function toggleGettingStartedCategoryContent(category) {
     const installationCatBtn = document.getElementById('installation-cat-btn');
@@ -107,3 +146,52 @@
         console.error("One or more Getting Started category content cards not found on the page.");
     }
  }
+
+
+ //toggle the display of the dynamic web hosting category content based on the button clicked
+ function toggleDynamicWebHostingCategoryContent(category) {
+    const introBtn = document.getElementById('dynamic-web-hosting-into-to-bashlets-btn');
+    const writingFirstBashletBtn = document.getElementById('dynamic-web-hosting-writing-first-bashlet-btn');
+    const contextBashletsBtn = document.getElementById('dynamic-web-hosting-context-bashlets-btn');
+    const examplesBashletsBtn = document.getElementById('dynamic-web-hosting-examples-bashlets-btn');
+
+    const introCard = document.getElementById('intro-to-bashlets-card');
+    const writingFirstBashletCard = document.getElementById('writing-your-first-bashlet-card');
+    const contextBashletCard = document.getElementById('the-bashlet-context-card');
+    const examplesBashletCard = document.getElementById('bashlet-examples-card');
+
+    // Remove active class from all nav-link elements
+    introBtn.querySelector('.nav-link').classList.remove('active');
+    writingFirstBashletBtn.querySelector('.nav-link').classList.remove('active');
+    contextBashletsBtn.querySelector('.nav-link').classList.remove('active');
+    examplesBashletsBtn.querySelector('.nav-link').classList.remove('active');
+
+    if(introCard && writingFirstBashletCard && contextBashletCard && examplesBashletCard){
+        if (category === 'intro') {
+            introCard.style.display = 'block';
+            writingFirstBashletCard.style.display = 'none';
+            contextBashletCard.style.display = 'none';
+            examplesBashletCard.style.display = 'none';
+            introBtn.querySelector('.nav-link').classList.add('active');
+        } else if (category === 'writing-first-bashlet') {
+            introCard.style.display = 'none';
+            writingFirstBashletCard.style.display = 'block';
+            contextBashletCard.style.display = 'none';
+            examplesBashletCard.style.display = 'none';
+            writingFirstBashletBtn.querySelector('.nav-link').classList.add('active');
+        }   else if (category === 'context') {  
+            introCard.style.display = 'none';
+            writingFirstBashletCard.style.display = 'none';
+            contextBashletCard.style.display = 'block';
+            examplesBashletCard.style.display = 'none';
+            contextBashletsBtn.querySelector('.nav-link').classList.add('active');
+        } else if (category === 'examples') {
+            introCard.style.display = 'none';
+            writingFirstBashletCard.style.display = 'none';
+            contextBashletCard.style.display = 'none';
+            examplesBashletCard.style.display = 'block';
+            examplesBashletsBtn.querySelector('.nav-link').classList.add('active');
+        }
+    }
+   
+}
